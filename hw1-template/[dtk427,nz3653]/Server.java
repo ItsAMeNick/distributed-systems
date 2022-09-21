52e787364d1ea13247a2f88d148fe973b6d44e35
Nicholas Zoner <nzoner@lutron.com>
2022-09-21 02:29:22 -0500

    Added setmode support and reformatted the files

--- hw1-template/[dtk427,nz3653]/Server.java
+++ hw1-template/[dtk427,nz3653]/Server.java
@@ -5,16 +5,16 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Server {
-  public static void main (String[] args) {
+    public static void main (String[] args) {
     int tcpPort;
     int udpPort;
     if (args.length != 3) {
-      System.out.println("ERROR: Provide 3 arguments");
-      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
-      System.out.println("\t(3) <file>: the file of inventory");
+        System.out.println("ERROR: Provide 3 arguments");
+        System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
+        System.out.println("\t(2) <udpPort>: the port number for UDP connection");
+        System.out.println("\t(3) <file>: the file of inventory");
 
-      System.exit(-1);
+        System.exit(-1);
     }
     tcpPort = Integer.parseInt(args[0]);
     udpPort = Integer.parseInt(args[1]);
@@ -41,5 +41,5 @@ public class Server {
     // Inventory Loaded
 
     // TODO: handle request from clients
-  }
+    }
 }
